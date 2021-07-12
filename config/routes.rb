ef4de85227939cc1e341a791db95d40dc34e3054
Enio N. de Souza <enionsouza@gmail.com>
2021-07-12 15:21:29 -0400

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/about'
+  root to: 'home#index'
   resources :groceries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
