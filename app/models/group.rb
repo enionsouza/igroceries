e798b5f85362b414aaa82a6a58f0328a94d5e47e
Enio N. de Souza <enionsouza@gmail.com>
2021-07-13 13:16:16 -0400

    Add active record validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,22 @@
+require 'uri'
+require 'net/http'
+
 class Group < ApplicationRecord
   belongs_to :user
 
   has_and_belongs_to_many :groceries
+
+  validates :user_id, :name, presence: true
+  validate :icon_is_image?
+
+  def icon_is_image?
+    return true if icon.nil?
+
+    url = URI.parse(icon)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = (url.scheme == 'https')
+    http.start do |session|
+      errors.add(:icon, 'is not a valid image') unless session.head(url.request_uri)['Content-Type'].start_with? 'image'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { in: 3..20 }
+
   has_many :groups
   has_many :groceries, foreign_key: :author_id
 end
